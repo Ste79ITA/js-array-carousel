19190c6056884395f7e62ef52f8ae32cb331042a
Ste79ITA <nastri.s@gmail.com>
2023-10-10 16:22:53 +0200

    Cycle FOR Refactored

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+// DOM SElectors
 const carouselDOMElement = document.querySelector('.carousel');
+const forwardBtnDOMElement = document.querySelector('.forward');
+const backwardBtnDOMElement = document.querySelector('.backward');
 
 // Inserire immagini carosello in un array
 const imageArray = [
@@ -10,14 +13,16 @@ const imageArray = [
 ];
 
 //   Utilizzare un ciclo FOR per popolare il carosello con le immagini, solo l'immagine con la classe display verrà mostrata.
+let imgDisplay = 0;
 
 for (let i = 0; i < imageArray.length; i++) {
-  let imgClass = 'hide';
-  let imgDisplay = 0;
-
-  carouselDOMElement.innerHTML += `<img class=${imgClass} src=${imageArray[i]} >`;
-
-  if (imageArray[i] === imgDisplay) {
-    imgClass = 'display';
+  if (i === imgDisplay) {
+    carouselDOMElement.innerHTML += `<img class=${'display'} src=${
+      imageArray[i]
+    } >`;
+  } else {
+    carouselDOMElement.innerHTML += `<img class=${'hide'} src=${
+      imageArray[i]
+    } >`;
   }
 }
