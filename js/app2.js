da7a6827048176e05dceacda75c01b5939455372
Ste79ITA <nastri.s@gmail.com>
2023-10-11 18:23:12 +0200

    JS Refactored

--- js/app2.js
+++ js/app2.js
@@ -36,43 +36,25 @@ const forwardBtnDOMElement = document.querySelector('.forward');
 const backwardBtnDOMElement = document.querySelector('.backward');
 
 forwardBtnDOMElement.addEventListener('click', function () {
-  // RIMUOVE CLASSE DISPLAY DALL' ELEMENTO CORRENTE
-  currentSlide.classList.remove('display');
-  currentThumb.classList.remove('display');
-  // SE INDICE MINORE DI 4
-  if (currentIndex < 4) {
-    // AGGIUNGE CLASSI ALL'ELEMENTO SUCCESSIVO
-    slides[currentIndex + 1].classList.add('display');
-    thumbs[currentIndex + 1].classList.add('display');
-    // AUMENTA IL VALORE DELL'INDICE DI 1
-    currentSlide = slides[(currentIndex += 1)];
-    currentThumb = thumbs[currentIndex];
+  slides[currentIndex].classList.remove('display');
+  thumbs[currentIndex].classList.remove('display');
+  if (currentIndex === slides.length - 1) {
+    currentIndex = 0;
   } else {
-    // SI RIPARTE DALL'INDICE 0
-    slides[(currentIndex = 0)].classList.add('display');
-    thumbs[(currentIndex = 0)].classList.add('display');
-    currentSlide = slides[(currentIndex = 0)];
-    currentThumb = thumbs[currentIndex];
+    currentIndex++;
   }
+  slides[currentIndex].classList.add('display');
+  thumbs[currentIndex].classList.add('display');
 });
 
 backwardBtnDOMElement.addEventListener('click', function () {
-  // RIMUOVE CLASSE DISPLAY DALL' ELEMENTO CORRENTE
-  currentSlide.classList.remove('display');
-  currentThumb.classList.remove('display');
-  // SE INDICE MAGGIORE DI 0
-  if (currentIndex > 0) {
-    // AGGIUNGE CLASSI ALL'ELEMENTO PRECEDENTE
-    slides[currentIndex - 1].classList.add('display');
-    thumbs[currentIndex - 1].classList.add('display');
-    // RIDUCE IL VALORE DELL'INDICE DI 1
-    currentSlide = slides[(currentIndex -= 1)];
-    currentThumb = thumbs[currentIndex];
+  slides[currentIndex].classList.remove('display');
+  thumbs[currentIndex].classList.remove('display');
+  if (currentIndex === 0) {
+    currentIndex = slides.length - 1;
   } else {
-    // SI RIPARTE DALL'ULTIMO INDICE
-    slides[(currentIndex = slides.length - 1)].classList.add('display');
-    thumbs[(currentIndex = slides.length - 1)].classList.add('display');
-    currentSlide = slides[(currentIndex = slides.length - 1)];
-    currentThumb = thumbs[currentIndex];
+    currentIndex--;
   }
+  slides[currentIndex].classList.add('display');
+  thumbs[currentIndex].classList.add('display');
 });
